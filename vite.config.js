c1b5b407086cc9e263b7fae4fb0717f036d53def
yuzi <125455495+Anko-11@users.noreply.github.com>
2023-10-13 17:18:15 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,56 @@
+import { defineConfig,loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
+import { createStyleImportPlugin } from 'vite-plugin-style-import'
+import { resolve } from 'path'
+// https://vitejs.dev/config/
+export default ({command, mode}) => defineConfig({
+	plugins: [
+	  vue(),
+	  Components({
+	     resolvers: [VantResolver()],
+	   }),
+	   	createStyleImportPlugin({
+	   	      resolves: [{
+	   	          libraryName: '@nutui/nutui',
+	   	          libraryNameChangeCase: 'pascalCase',
+	   	          resolveStyle: (name) => {
+	   	            return `@nutui/nutui/dist/packages/${name.toLowerCase()}/index.scss`
+	   	          },
+	   	      }]
+	   	}),
+	],
+	css: {
+	    preprocessorOptions: {
+	      scss: {
+	        // 配置 nutui 全局 scss 变量
+	        additionalData: `@import "@nutui/nutui/dist/styles/variables.scss";`
+	      }
+	    }
+	  },
+	resolve: {
+	     alias: [
+			{ find: '@', replacement: resolve(__dirname, 'src') },
+			{ find: '_c', replacement: resolve(__dirname, 'src/components') },
+			{ find: '_v', replacement: resolve(__dirname, 'src/views') },
+			{ find: '_b', replacement: resolve(__dirname, 'src/components/HelloWorld.vue') },
+		]
+	  },
+	server: {
+		// host: true,
+		// hmr: true,
+		proxy: {
+			'/api': {
+				// 根据不同运行环境加载不同的配置文件
+				target: loadEnv(mode, process.cwd()).VITE_API_TARGET_URL,
+				// 根据配置文件加载
+				// target: serverConfig.server.dev,
+				changeOrigin: true,
+				rewrite: (path) => path.replace(/^\/api/, '')
+			}
+		}
+	},
+
+
+})
